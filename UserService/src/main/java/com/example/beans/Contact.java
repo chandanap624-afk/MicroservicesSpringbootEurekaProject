87c8c7f40bc940106c1b1b3e4f44d019e7ab860f
chandanap624-afk <chandanap624@gmail.com>
2026-05-20 22:59:09 +0530

    Add files via upload

---
+++ UserService/src/main/java/com/example/beans/Contact.java
@@ -0,0 +1,21 @@
+package com.example.beans;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Remove the JPA annotations because we don't want to save in the database through repository because it's like a DTO
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+	
+	// Here we are using only for getter and setter methods 
+
+	Integer conId;
+	String email;
+	String phone;
+	Integer userId;
+
+}
